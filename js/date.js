3894e2beb92e784ec7c8763666876d41907cfc5c
Biaa52 <168538498+Biaa52@users.noreply.github.com>
2024-12-25 02:11:02 +0200

    Add files via upload

---
+++ js/date.js
@@ -0,0 +1,17 @@
+let hour=document.getElementById("hour");
+let min=document.getElementById("min");
+let sec=document.getElementById("sec");
+
+
+
+setInterval(() => {
+    let currentDate=new Date();
+
+hour.innerHTML=currentDate.getHours();
+min.innerHTML=currentDate.getMinutes();
+sec.innerHTML=currentDate.getSeconds();
+}, 1000);
+
+
+ 
+
